9c7b32673a7e340e1ffb869f3030fe195d6c89f4
calumsmyth <calumsmyth@hotmail.co.uk>
2025-01-09 16:19:08 +0000

    Completed Exercises

---
+++ python_basics/python_exercise.py
@@ -0,0 +1,19 @@
+#If Statement
+
+#Exercise 1 - Check if number is even
+num = 8
+if num % 2 == 0:
+    print("The number is even")
+
+
+#Exercise 2 - Display a message if a list is empty
+list = []
+if len(list) == 0:
+    print("List is empty")
+
+
+
+
+#else Statement
+
+#Exercise 1 - Is number odd or even
