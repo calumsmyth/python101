03ec753b2b5e10c47612531a4acecdd4b4712335
calumsmyth <calumsmyth@hotmail.co.uk>
2025-01-14 15:46:25 +0000

    Simple library exercise complete

--- Week 2/class.py
+++ Week 2/class.py
@@ -1,23 +1,72 @@
-class student:
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade
+#Build a Simple Library System
 
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.available = True
     def __str__(self):
-        return f"Student Name: {self.name}, Age: {self.age}, Grade: {self.grade}."
+        return f"Book Title: {self.title}, Author: {self.author}"
 
-    def update_grade(self, new_grade):
-        self.grade = new_grade
+class Member:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.borrowed_books = []
+    def __str__(self):
+        return f"Member Name: {self.name}, Member ID: {self.id}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+        self.members = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def register_member(self, member):
+        self.members.append(member)
+
+    def lend_book(self, book, member):
+        if book.available:
+            book.available = False
+            member.borrowed_books.append(book)
+            print(f"{book.title} has been lent to {member.name}.")
+        else:
+            print(f"Sorry {book.title} is unavailable to borrow")
+
+    def return_book(self, book, member):
+        if book in member.borrowed_books:
+            book.available = True
+            member.borrowed_books.remove(book)
+            print(f"{book.title} has been returned by {member.name}")
+        else:
+            print(f"{member.name} did not borrow {book.title}")
+    
+
+#create books
+book1= Book("Harry Potter", "J. K. Rowling")
+book2 = Book("To Kill a Mockingbird", "Harper Lee")
+
+#Create Members
+member1 = Member("John", 101)
+member2 = Member("Alice", 102)
+
+#create library
+library = Library()
+
+#add books to library
+library.add_book(book1)
+library.add_book(book2)
+
+#add members
+library.register_member(member1)
+library.register_member(member2)
+
+#test lending and returning books
+#library.lend_book(book1,member1)
+library.lend_book(book1, member1)
+library.lend_book(book1, member2)
 
 
-student1 = student("Alice", 20, "A")
 
-print(student1.name)
-print(student1.age)
-print(student1.grade)
-
-print(student1)
-student1.update_grade("A+")
-
-print(student1)
